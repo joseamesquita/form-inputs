7be8dfabb433145bf813fcd4545bdafb85c08db7
Jose Amesquita and Kristina Hengster Tintor <jose@>
2020-01-15 14:17:16 -0800

    form input types

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("form#beverage").submit(function(event) {
+    var beverage = $("#beverage").val();
+    // var beverage = parseInt($("#beverage").val()); //integer
+  });
+});
